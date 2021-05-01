7ac73f4757219fef7adf88216833791264de10e6
sfarouk3 <54846533+sfarouk3@users.noreply.github.com>
2021-05-01 23:12:39 +0200

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -4,13 +4,13 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-'''
-Input:
-messages, the messages received from different channels after the disaster
-categories, the messages corresponding categories
-Output:
-df the data file after merging the messages and categories tables
-'''
+    '''
+    Input:
+    messages, the messages received from different channels after the disaster
+    categories, the messages corresponding categories
+    Output:
+    df the data file after merging the messages and categories tables
+    '''
 
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
@@ -20,18 +20,18 @@ df the data file after merging the messages and categories tables
 
 
 def clean_data(df):
-'''
-Input: 
-df the data file after merging the messages and categories tables
-Output:
-df after cleaning the data, splitting categories columns , giving it new names, adjusting the values to be 0s and 1s and removing duplicates
-'''
+    '''
+    Input: 
+    df the data file after merging the messages and categories tables
+    Output:
+    df after cleaning the data, splitting categories columns , giving it new names, adjusting the values to be 0s and 1s and removing duplicates
+    '''
     categories = df['categories'].str.split(";" , expand=True)
     row = categories.iloc[0]
 
-	# use this row to extract a list of new column names for categories.
-	# one way is to apply a lambda function that takes everything 
-	# up to the second to last character of each string with slicing
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
     category_colnames =row.str.split("-", expand=True).iloc[:, 0]
     # rename the columns of `categories`
     categories.columns = category_colnames
@@ -53,11 +53,11 @@ df after cleaning the data, splitting categories columns , giving it new names, 
 
 
 def save_data(df, database_filename):
-'''
-Input: 
-df: the data file the needs to be saved
-database_filename: the database name
-'''
+    '''
+    Input: 
+    df: the data file the needs to be saved
+    database_filename: the database name
+    '''
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('messages_categories', engine, index=False, if_exists='replace') 
 
